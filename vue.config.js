aaca3d3274f960d188f8cca40e5663e52623970f
Camilo De la Cruz <delacruz.arboleda@gmail.com>
2022-10-20 01:24:30 -0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: './'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/delarbole/'
+    : '/'
 })
+
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   publicPath: './'
+// })
