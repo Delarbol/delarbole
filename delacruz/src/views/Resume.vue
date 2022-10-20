eefc2bc8afe96ab5b80c2942d555a5f63fc8a9b4
Camilo De la Cruz <delacruz.arboleda@gmail.com>
2022-10-20 00:58:13 -0500

    update
    Using template

---
+++ delacruz/src/views/Resume.vue
@@ -0,0 +1,203 @@
+<template>
+  <v-layout row justify-center align-center wrap class="mt-4 pt-2">
+    <v-flex xs12 sm12 md5 lg6 xl6 class="mt-4 pt-4">
+      <h2 class="mb-4 pl-4 pt-2">Educación</h2>
+      <v-card-text class="py-0">
+        <v-timeline align-top dense>
+          <v-timeline-item color="green" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold green--text">JUN 2020 -</div>
+                <div class="caption font-weight-bold green--text">OCT 2020</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Data Science 4 All</strong>
+                <div class="caption mb-2">Correlation One</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+          <v-timeline-item color="green" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold green--text">AGO 2017 -</div>
+                <div class="caption font-weight-bold green--text">MAY 2018</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Master of Laws</strong>
+                <div class="caption mb-2">University of California, Berkeley</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+          <v-timeline-item color="green" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold green--text">ENE 2008 -</div>
+                <div class="caption font-weight-bold green--text">DIC 2013</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Lawyer</strong>
+                <div class="caption mb-2">Universidad Externado de Colombia</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+        </v-timeline>
+      </v-card-text>
+    </v-flex>
+    <v-flex xs12 sm12 md4 lg6 xl6 class="mt-4 pt-2">
+      <h2 class="mb-4 pl-4">Experiencia</h2>
+      <v-card-text class="py-0">
+        <v-timeline align-top dense>
+          <v-timeline-item color="green" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold green--text">NOV 2021 -</div>
+                <div class="caption font-weight-bold green--text">PRESENT</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Data Scientist - Marketing VP</strong>
+                <div class="caption mb-2">Bancolombia - Remote</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+          <v-timeline-item color="green" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold green--text">FEB 2014 -</div>
+                <div class="caption font-weight-bold green--text">PRESENT</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Professor</strong>
+                <div class="caption mb-2">Universidad Externado de Colombia</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+          <v-timeline-item color="green" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold green--text">JAN 2014 -</div>
+                <div class="caption font-weight-bold green--text">JUN 2015</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Junior Lawyer</strong>
+                <div class="caption mb-2">APHS</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+        </v-timeline>
+      </v-card-text>
+    </v-flex>
+    <v-layout row justify-center align-center wrap class="mt-4 pt-2">
+      <v-flex xs12 sm12 md4 lg5 xl5 class="mx-2 pt-4">
+        <h2>
+          <span>Soft</span>
+
+          <span class="green--text">Skills</span>
+        </h2>
+        <br>
+        <strong>Problem Solving</strong>
+        <v-progress-linear v-model="buffer1" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+        <strong>Adaptability</strong>
+        <v-progress-linear v-model="buffer2" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+        <strong>Teamwork</strong>
+        <v-progress-linear v-model="buffer3" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+      </v-flex>
+      <v-flex xs12 sm12 md4 lg5 xl5 class="mx-2 pt-4">
+        <h2>
+          <span>Coding</span>
+          <span class="green--text">Skills</span>
+        </h2>
+        <br>
+        <strong>Python</strong>
+        <v-progress-linear v-model="buffer4" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+        <strong>Algorithmic Thinking</strong>
+        <v-progress-linear v-model="buffer5" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+        <strong>Creativity</strong>
+        <v-progress-linear v-model="buffer6" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+      </v-flex>
+      <div class="mt-4 pt-2">
+        <v-btn
+          disabled
+          href="/"
+          target="_blank"
+          color="green"
+          dark
+          large
+        >
+          Download Resume
+          <v-icon right dark>fas fa-file-download</v-icon>
+        </v-btn>
+      </div>
+    </v-layout>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Resume",
+    titleTemplate: "%s ← Camilo's Page",
+    meta: [
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        name: "description",
+        content:
+          "Vue NUXT HTML CSS JavaScript Design Adobe XD Photoshop Coding Skills Resume Work Web Developer Front-End"
+      },
+      { charset: "utf-8" },
+      { property: "og:title", content: "Eldin' Space" },
+      { property: "og:site_name", content: "Eldin' Space" },
+      { property: "og:type", content: "website" },
+      { property: "og:url", content: "https://eldin.space" },
+      {
+        property: "og:image",
+        content: "https://i.imgur.com/Dcz2PGx.jpg"
+      },
+      {
+        property: "og:description",
+        content:
+          "Vue NUXT HTML CSS JavaScript Design Adobe XD Photoshop Coding Skills Resume Work Web Developer Front-End"
+      }
+    ]
+  },
+  data() {
+    return {
+      value: 0,
+      buffer1: 0,
+      buffer2: 0,
+      buffer3: 0,
+      buffer4: 0,
+      buffer5: 0,
+      buffer6: 0,
+      bufferValue: 100,
+      interval: 0
+    };
+  },
+  mounted() {
+    this.startBuffer();
+  },
+
+  beforeDestroy() {
+    clearInterval(this.interval);
+  },
+
+  methods: {
+    startBuffer() {
+      this.interval = setInterval(() => {
+        this.buffer1 = 90;
+        this.buffer2 = 85;
+        this.buffer3 = 80;
+        this.buffer4 = 80;
+        this.buffer5 = 75;
+        this.buffer6 = 85;
+      }, 800);
+    }
+  }
+};
+</script>
+<style >
+</style>
+
