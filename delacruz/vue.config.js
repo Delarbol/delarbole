c0d5fee8674aa308688727e5951504c0c8116bbe
Camilo De la Cruz <delacruz.arboleda@gmail.com>
2022-10-20 01:00:58 -0500

    Update vue.config.js

--- delacruz/vue.config.js
+++ delacruz/vue.config.js
@@ -1,5 +1,5 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true,
-//   // publicPath: './'
-// })
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: './'
+})
